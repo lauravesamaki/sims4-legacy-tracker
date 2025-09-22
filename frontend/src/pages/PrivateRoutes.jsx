f79916e068252e16c0c45f76a295767dc72658cb
Laura Vesamäki <laura.vesamaki@live.fi>
2025-09-22 14:15:18 +0300

    modified privateroute authentication

--- frontend/src/pages/PrivateRoutes.jsx
+++ frontend/src/pages/PrivateRoutes.jsx
@@ -1,9 +1,35 @@
 import { Navigate, Outlet } from 'react-router-dom'
+import { useEffect, useState } from "react"
 
 export default function PrivateRoutes() {
-    const token = sessionStorage.getItem('token')
+    const [loading, setLoading] = useState(true)
+    const [isAuth, setIsAuth] = useState(false)
 
+    useEffect(() => {
+        async function checkAuth() {
+            try {
+                const response = await fetch("/auth-check", {
+                    method: "GET",
+                    credentials: "include"
+                })
+
+                if (response.ok) {
+                    setIsAuth(true)
+                } else {
+                    setIsAuth(false)
+                }
+            } catch {
+                setIsAuth(false)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        checkAuth()
+    }, [])
+
+    if (loading) return <div>Loading...</div>
     return (
-        token ? <Outlet /> : <Navigate to='/login' />
+        isAuth ? <Outlet /> : <Navigate to='/login' />
     )
 }
