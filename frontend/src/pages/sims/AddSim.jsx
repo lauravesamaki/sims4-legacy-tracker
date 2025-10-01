e4c4300faf1c9e5e6d5eb3657c2c159c7c98ac2b
Laura Vesamäki <laura.vesamaki@live.fi>
2025-10-01 15:24:13 +0300

    added translations for buttons in addsim and editsim pages

--- frontend/src/pages/sims/AddSim.jsx
+++ frontend/src/pages/sims/AddSim.jsx
@@ -1,10 +1,13 @@
 import SimForm from "../../components/SimForm"
+import { useTranslation } from "react-i18next"
 
 export default function AddSim() {
+    const {t} = useTranslation()
+
     return (
         <div class="container-fluid mt-4 card card-add-sim text-bg-dark mb-4">
             <div class="card-body">
-                <h3 class="mb-4 card-header">Add New Sim</h3>
+                <h3 class="mb-4 card-header">{t('addSim')}</h3>
                 <div class="card-text">
                     <SimForm props={{path: "add"}} />
                 </div>
--- frontend/src/pages/sims/EditSim.jsx
+++ frontend/src/pages/sims/EditSim.jsx
@@ -1,12 +1,15 @@
 import SimForm from "../../components/SimForm"
 import { useLocation } from "react-router-dom"
+import { useTranslation } from "react-i18next"
 
 export default function EditSim({props}) {
+    const {t} = useTranslation()
     const data = useLocation()
+
     return <>
         <div class="container-fluid mt-4 card card-add-sim text-bg-dark mb-4">
             <div class="card-body">
-                <h3 class="mb-4 card-header">Edit Sim</h3>
+                <h3 class="mb-4 card-header">{t('editSim')}</h3>
                 <div class="card-text">
                     <SimForm props={{path: "edit", sim: data}} />
                 </div>
